9afdb1c82c829ec52b2451e9e90ebeca94ae7bbb
lukasz protosawicki <lukas.es.p@o2.pl>
2021-07-18 21:16:05 +0200

    add slider.js and mixed main.scss

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,12 @@
+import slider from "../img/slider.jpg";
+
 function Slider() {
-  return <div>slider</div>;
+  return (
+    <div className="slider">
+      <img src={slider} alt="layout" />
+      <span></span>
+    </div>
+  );
 }
 
 export default Slider;
