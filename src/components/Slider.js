25510919f060bcbc6951a0ede09b2f4c216587e2
lukasz protosawicki <lukas.es.p@o2.pl>
2021-07-20 00:44:52 +0200

    add styles

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,6 +4,13 @@ function Slider() {
   return (
     <div className="slider">
       <img src={slider} alt="layout" />
+      <ul className="position_slider">
+        <span></span>
+        <span></span>
+        <span className="slider_color"></span>
+        <span></span>
+        <span></span>
+      </ul>
       <span></span>
     </div>
   );
